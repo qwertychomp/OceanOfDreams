9285c32501ad01c0d113f54ad05b03cd113b8e94
qwertychomp <supersequoia11@gmail.com>
2024-01-18 07:12:22 -0800

    keybind system (il add player when we have art)

---
+++ src/keybindHandler.gd
@@ -0,0 +1,70 @@
+extends Node
+#keybind code, ripped directly from my ogpc game 
+@onready var actions:Array = []
+var Parse:JSON = JSON.new()
+func makeDefaultKeybinds()->void:
+	if(!FileAccess.file_exists("user://keybinds.json")):
+		var parse:JSON = JSON.new()
+		var defaultkeybinds = FileAccess.open("res://defaultConfig/keybinds.json",FileAccess.READ)
+		var userKeybindFile = FileAccess.open("user://keybinds.json",FileAccess.WRITE)
+		userKeybindFile.store_string(defaultkeybinds.get_as_text())
+func addInputAction(key:String,actionName:String):
+	var donger = OS.find_keycode_from_string(key)
+	var dingdongdingalongadingdongdee:InputEventKey = InputEventKey.new()
+	dingdongdingalongadingdongdee.physical_keycode = donger
+	InputMap.add_action(actionName)
+	InputMap.action_add_event(actionName,dingdongdingalongadingdongdee)
+  
+func Unparse():
+	pass
+func changeKeybind(actionName:String,keybindNumber:int,newKey:String)->bool:
+	var bufferParse:JSON = JSON.new()
+	var lerawjson = FileAccess.open("user://keybinds.json",FileAccess.READ)
+	var lejson = lerawjson.get_as_text()
+	bufferParse.parse(lejson)
+	
+	var leJsonBuffer = bufferParse.data
+	lerawjson.close()
+	
+	var foundActionNumber = -1
+	for bhhbj in leJsonBuffer["keybinds"].size():
+		if(leJsonBuffer["keybinds"][bhhbj]["actionName"]==actionName):
+			foundActionNumber = bhhbj
+			break
+			
+	if(foundActionNumber == -1):
+		return false
+		
+	leJsonBuffer["keybinds"][foundActionNumber]["key"][keybindNumber] = newKey
+	lerawjson = FileAccess.open("user://keybinds.json",FileAccess.WRITE)
+	lerawjson.store_string(bufferParse.stringify(leJsonBuffer,"\t"))
+	lerawjson.close()
+	reloadKeybinds()
+	return true
+	
+func reloadKeybinds():
+	if(!FileAccess.file_exists("user://keybinds.json")):
+		var g = FileAccess.open("user://keybinds.json",FileAccess.WRITE)
+		var res = FileAccess.open("res://defaultConfig/keybinds.json",FileAccess.READ)
+		g.store_string(res.get_as_text())
+		g.close()
+		res.close()
+		
+	var keybindConfig = FileAccess.open("user://keybinds.json",FileAccess.READ)
+	var configText = keybindConfig.get_as_text()
+	actions = InputMap.get_actions()
+	
+	for d in actions.size():
+		InputMap.erase_action(actions[d])
+		
+	Parse.parse(configText)
+	var parseData = Parse.data
+	for ygu in parseData["keybinds"].size():
+		for hbnnkjjijo in parseData["keybinds"][ygu]["key"].size():
+			addInputAction(parseData["keybinds"][ygu]["key"][hbnnkjjijo],parseData["keybinds"][ygu]["actionName"])
+		
+	keybindConfig.close()
+
+func _ready():
+	makeDefaultKeybinds()
+	reloadKeybinds()
